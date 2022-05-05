643465130bc509e88dbeae86d195a3f0e9bf5dee
freakyfaces <70147234+freakyfaces@users.noreply.github.com>
2022-05-05 12:33:38 +0430

    setting numerical keyboard for getting coordinates

--- src/app/src/main/java/edu/sharif/ce/mas/weather/CoordinatesFragment.java
+++ src/app/src/main/java/edu/sharif/ce/mas/weather/CoordinatesFragment.java
@@ -1,20 +1,27 @@
 package edu.sharif.ce.mas.weather;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
 public class CoordinatesFragment extends Fragment {
 
+    EditText xInput;
+    EditText yInput;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_city, container
                 , false);
+        xInput = root.findViewById(R.id.xinput);
+        yInput = root.findViewById(R.id.yinput);
         return root;
     }
 }
